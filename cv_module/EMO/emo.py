18312ef77514426793f67a7f63c4107ac3aff37d
Saito912 <ct990912@outlook.com>
2023-09-17 00:21:31 +0800

    add ICCV23-EMO Moudle iRMB, 轻量级

---
+++ cv_module/EMO/emo.py
@@ -0,0 +1,113 @@
+from einops import rearrange, reduce
+from timm.models.layers.activations import *
+from timm.models.layers import DropPath
+from timm.models.efficientnet_blocks import SqueezeExcite as SE
+from cv_module.EMO.basic_modules import get_norm, get_act, ConvNormAct
+
+
+inplace = True
+
+# 论文讲解
+# https://mp.weixin.qq.com/s?__biz=MzI5MDUyMDIxNA==&mid=2247653815&idx=1&sn=6a307058866c10cf2d5c2b46c8a0e854&chksm=ec12744edb65fd58b890421ea0410ac1ce22e41ff747a62524e83ea76db71ccb9309e04a9a64&token=738485724&lang=zh_CN#rd
+
+
+class iRMB(nn.Module):
+
+    def __init__(self, dim_in, dim_out, norm_in=True, has_skip=True, exp_ratio=1.0, norm_layer='bn_2d',
+                 act_layer='relu', v_proj=True, dw_ks=3, stride=1, dilation=1, se_ratio=0.0, dim_head=64, window_size=7,
+                 attn_s=True, qkv_bias=False, attn_drop=0., drop=0., drop_path=0., v_group=False, attn_pre=False):
+        super().__init__()
+        self.norm = get_norm(norm_layer)(dim_in) if norm_in else nn.Identity()
+        dim_mid = int(dim_in * exp_ratio)
+        self.has_skip = (dim_in == dim_out and stride == 1) and has_skip
+        self.attn_s = attn_s
+        if self.attn_s:
+            assert dim_in % dim_head == 0, 'dim should be divisible by num_heads'
+            self.dim_head = dim_head
+            self.window_size = window_size
+            self.num_head = dim_in // dim_head
+            self.scale = self.dim_head ** -0.5
+            self.attn_pre = attn_pre
+            self.qk = ConvNormAct(dim_in, int(
+                dim_in * 2), kernel_size=1, bias=qkv_bias, norm_layer='none', act_layer='none')
+            self.v = ConvNormAct(dim_in, dim_mid, kernel_size=1, groups=self.num_head if v_group else 1,
+                                 bias=qkv_bias, norm_layer='none', act_layer=act_layer, inplace=inplace)
+            self.attn_drop = nn.Dropout(attn_drop)
+        else:
+            if v_proj:
+                self.v = ConvNormAct(dim_in, dim_mid, kernel_size=1, bias=qkv_bias,
+                                     norm_layer='none', act_layer=act_layer, inplace=inplace)
+            else:
+                self.v = nn.Identity()
+        self.conv_local = ConvNormAct(dim_mid, dim_mid, kernel_size=dw_ks, stride=stride,
+                                      dilation=dilation, groups=dim_mid, norm_layer='bn_2d', act_layer='silu', inplace=inplace)
+        self.se = SE(dim_mid, rd_ratio=se_ratio, act_layer=get_act(
+            act_layer)) if se_ratio > 0.0 else nn.Identity()
+
+        self.proj_drop = nn.Dropout(drop)
+        self.proj = ConvNormAct(dim_mid, dim_out, kernel_size=1,
+                                norm_layer='none', act_layer='none', inplace=inplace)
+        self.drop_path = DropPath(drop_path) if drop_path else nn.Identity()
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm(x)
+        B, C, H, W = x.shape
+        if self.attn_s:
+            # padding
+            if self.window_size <= 0:
+                window_size_W, window_size_H = W, H
+            else:
+                window_size_W, window_size_H = self.window_size, self.window_size
+            pad_l, pad_t = 0, 0
+            pad_r = (window_size_W - W % window_size_W) % window_size_W
+            pad_b = (window_size_H - H % window_size_H) % window_size_H
+            x = F.pad(x, (pad_l, pad_r, pad_t, pad_b, 0, 0,))
+            n1, n2 = (H + pad_b) // window_size_H, (W + pad_r) // window_size_W
+            x = rearrange(
+                x, 'b c (h1 n1) (w1 n2) -> (b n1 n2) c h1 w1', n1=n1, n2=n2).contiguous()
+            # attention
+            b, c, h, w = x.shape
+            qk = self.qk(x)
+            qk = rearrange(qk, 'b (qk heads dim_head) h w -> qk b heads (h w) dim_head',
+                           qk=2, heads=self.num_head, dim_head=self.dim_head).contiguous()
+            q, k = qk[0], qk[1]
+            attn_spa = (q @ k.transpose(-2, -1)) * self.scale
+            attn_spa = attn_spa.softmax(dim=-1)
+            attn_spa = self.attn_drop(attn_spa)
+            if self.attn_pre:
+                x = rearrange(
+                    x, 'b (heads dim_head) h w -> b heads (h w) dim_head', heads=self.num_head).contiguous()
+                x_spa = attn_spa @ x
+                x_spa = rearrange(x_spa, 'b heads (h w) dim_head -> b (heads dim_head) h w',
+                                  heads=self.num_head, h=h, w=w).contiguous()
+                x_spa = self.v(x_spa)
+            else:
+                v = self.v(x)
+                v = rearrange(
+                    v, 'b (heads dim_head) h w -> b heads (h w) dim_head', heads=self.num_head).contiguous()
+                x_spa = attn_spa @ v
+                x_spa = rearrange(x_spa, 'b heads (h w) dim_head -> b (heads dim_head) h w',
+                                  heads=self.num_head, h=h, w=w).contiguous()
+            # unpadding
+            x = rearrange(
+                x_spa, '(b n1 n2) c h1 w1 -> b c (h1 n1) (w1 n2)', n1=n1, n2=n2).contiguous()
+            if pad_r > 0 or pad_b > 0:
+                x = x[:, :, :H, :W].contiguous()
+        else:
+            x = self.v(x)
+
+        x = x + self.se(self.conv_local(x)
+                        ) if self.has_skip else self.se(self.conv_local(x))
+
+        x = self.proj_drop(x)
+        x = self.proj(x)
+
+        x = (shortcut + self.drop_path(x)) if self.has_skip else x
+        return x
+
+
+if __name__ == '__main__':
+    import torch
+    from cv_module.model_test import model_test
+    model_test(iRMB(256, 256), (torch.randn(1, 256, 32, 32),))
--- cv_module/__init__.py
+++ cv_module/__init__.py
@@ -1,5 +1,5 @@
 from .ASFF.asff import ASFF
 from .RFB.rfb import BasicRFB
+from .EMO.emo import iRMB
 
-
-__all__ = ['ASFF', 'BasicRFB']
+__all__ = ['ASFF', 'BasicRFB', 'iRMB']
