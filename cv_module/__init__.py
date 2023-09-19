9dcc84c2741c6f28880a82df40ca345e56922a6f
Saito912 <ct990912@outlook.com>
2023-09-20 00:27:24 +0800

    add seaformer

--- cv_module/__init__.py
+++ cv_module/__init__.py
@@ -3,7 +3,10 @@ from .RFB.rfb import BasicRFB
 from .EMO.emo import iRMB
 from .FasterViT.faster_vit import FasterViTLayer
 from .Ghost.ghost import Ghost, GhostConv
-__all__ = ['ASFF', 'BasicRFB', 'iRMB', 'FasterViTLayer', 'Ghost', 'GhostConv']
+from .SeaFormer.seaformer import Sea_Attention, Sea_Attention_Block
+
+__all__ = ['ASFF', 'BasicRFB', 'iRMB', 'FasterViTLayer',
+           'Ghost', 'GhostConv', 'Sea_Attention', 'Sea_Attention_Block']
 
 """
 iRBM无效果,
--- cv_module/model_test.py
+++ cv_module/model_test.py
@@ -1,13 +1,16 @@
 from thop import profile
+from torch import Tensor
 from torch.nn import Module
-from typing import Tuple, Any
+from typing import Tuple, Any, Union
+from fvcore.nn import FlopCountAnalysis, flop_count_table
 
 
-def model_test(model: Module, input_data: Tuple[Any]):
+def model_test(model: Module, input_data: Union[Tensor, Tuple[Tensor, ...]]):
     # 创建一个随机输入张量，模拟模型的输入
     # 使用thop.profile来计算FLOPs和参数数量
-    flops, params = profile(model, inputs=input_data)
-    print(f"FLOPs: {flops / 1e9} G FLOPs")  # 以十亿FLOPs为单位打印
-    print(f"参数数量: {params / 1e6} 百万")
+    flops = FlopCountAnalysis(model, input_data)
+
+    print(flop_count_table(flops))
+
     out = model(*input_data)
     return out
